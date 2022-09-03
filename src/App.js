226571344cebf25bdc3e7921b8a4199a9dfd0a46
shubhamprateek <39755141+shubhamprateek@users.noreply.github.com>
2022-09-03 14:18:07 +0530

    2 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./style.css";
+import logo from './images/image.png'
+
 
 export default function App() {
   return (
     <div>
+      <img src = {logo}/>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
